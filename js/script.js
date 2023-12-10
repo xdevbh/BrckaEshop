ab879bb0a2f069cec708becfbe890f92d8a6a423
vladyslav.ralets <vladyslav.ralets@tacr-external.cz>
2023-12-10 17:32:20 +0100

    added mobile header

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-function menutoggle() {
-    var menuItems = document.getElementById("menuItems");
-    if (menuItems.style.maxHeight == "0px") {
-        menuItems.style.maxHeight = "300px";
-    } else {
-        menuItems.style.maxHeight = "0px";
-    }
-}
+// function menutoggle() {
+//     const menuItems = document.getElementById("menuItems");
+//     if (menuItems.style.maxHeight == "0px") {
+//         menuItems.style.maxHeight = "300px";
+//     } else {
+//         menuItems.style.maxHeight = "0px";
+//     }
+// }
 
 // function menutoggle() {
 //     var menuItems = document.getElementById("menuItems");
@@ -16,10 +16,21 @@ function menutoggle() {
 //     }
 // }
 
-function openSearch() {
-    document.getElementById("myOverlay").style.display = "block";
-}
+// function openSearch() {
+//     document.getElementById("myOverlay").style.display = "block";
+// }
 
-function closeSearch() {
-    document.getElementById("myOverlay").style.display = "none";
-}
+// function closeSearch() {
+//     document.getElementById("myOverlay").style.display = "none";
+// }
+
+const menuButton = document.querySelector(".header__menuIcon");
+const closeMenuButton = document.querySelector(".header__closeMenu");
+
+menuButton.addEventListener("click", () => {
+    document.querySelector(".header__mobMenu").classList.toggle("active");
+});
+
+closeMenuButton.addEventListener("click", () => {
+    document.querySelector(".header__mobMenu").classList.toggle("active");
+});
